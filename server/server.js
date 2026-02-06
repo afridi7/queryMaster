e375142ac714670e4c2860765daedcc1cd31620c
AbdulWaasaykhan <abdulwaasay001@gmail.com>
2026-02-06 11:58:02 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -157,4 +157,7 @@ io.on('connection', socket => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`server running on port ${PORT}`);
+});
